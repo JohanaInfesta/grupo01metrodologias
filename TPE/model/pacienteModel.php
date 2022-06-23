38e2f64140c30d7e0fc599ae19b4985f6a67fcbd
nahufalconaro <nfalconaro5@gmail.com>
2022-06-22 21:46:33 -0300

    MET 54 Get paciente by id

---
+++ TPE/model/pacienteModel.php
@@ -0,0 +1,20 @@
+<?php
+
+class pacienteModel {
+
+    private $dataBase;
+
+    /*
+    PARA QUE LA BASE DE DATOS FUNCIONE, SU NOMBRE DEBE COINCIDIR CON EL INDICADO EN EL ATRIBUTO...
+    */
+
+    public function __construct() {
+        $this->dataBase = new PDO('mysql:host=localhost;dbname=centro_medico', 'root', '');
+    }
+
+    //Funcion que obtiene un paciente especifico por DNI de la base de datos
+    function getPacienteById($id){
+        $query = $this->dataBase->prepare("SELECT * FROM paciente WHERE paciente.dni = ?");
+        $query->execute(array($id));
+        return $query->fetch(PDO::FETCH_OBJ);
+    }
