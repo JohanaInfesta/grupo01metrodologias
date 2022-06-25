a58b98091dd53a77d8f1df4867de613f36a006c1
Defelippekevin <kevin.defelippe@gmail.com>
2022-06-25 15:51:38 -0300

    MET-53

--- TPE/model/turnoModel.php
+++ TPE/model/turnoModel.php
@@ -3,7 +3,7 @@
         private $dataBase; 
         /* PARA QUE LA BASE DE DATOS FUNCIONE, SU NOMBRE DEBE COINCIDIR CON EL INDICADO EN EL ATRIBUTO... */ 
         public function __construct() { 
-            $this->dataBase = new PDO('mysql:host=localhost;dbname=centro_medico', 'root', 'kevin41123767'); 
+            $this->dataBase = new PDO('mysql:host=localhost;dbname=centro_medico', 'root', ''); 
         } 
         public function deleteTurno($id){ 
             $query = $this->dataBase->prepare("DELETE FROM turno WHERE turno.id = ?"); 
